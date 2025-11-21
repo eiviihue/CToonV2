a96325edca7ebdf460d8c5d9b51c556aa61cae96
Rafi Aqila Maulana <rafiaqila2005@gmail.com>
2025-11-22 00:23:17 +0700

    okkk

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -3,14 +3,12 @@ package controller;
 import dao.ComicDAO;
 import model.Comic;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/home")
 public class HomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
